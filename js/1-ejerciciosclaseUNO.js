ca9ba2ab72992e4fa6c241de4fe370d73d12ebf1
floralonso <floralonso@gmail.com>
2022-07-05 19:40:48 -0300

    Bucles

--- js/1-ejerciciosclaseUNO.js
+++
@@ -1,7 +0,0 @@
-alert('un mensaje');
-document.write('hello world');
-//document.write(3+5);
-//lo vimos en clase
-let nombre= prompt('Cual est tu nombre?');
-console.log(nombre)
-document.write('<br>Hola: ' + nombre)
--- js/8-ejerciciosClaseDOS.js
+++
@@ -1,7 +0,0 @@
-let numeroRandom = parseInt(prompt('Ingrese el numero para ver si es par'));
-
-if(numeroRandom%2==0){
-    document.write('Es divisible por dos')
-}else{
-    document.write('Elegir otro número')
-}
