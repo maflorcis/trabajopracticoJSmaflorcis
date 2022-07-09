9b83a2c04d4b1145d4d94b0877cca2c8ab82ea8f
floralonso <floralonso@gmail.com>
2022-07-09 16:35:09 -0300

    ejercicio 8

---
+++ js/buclesEjercicio8.js
@@ -0,0 +1,34 @@
+/*
+8- Crea script para generar pirámide siguiente con los números del 1 al número
+que indique el usuario (no mayor de 50)
+1
+12
+123
+1234
+12345
+123456
+……
+*/
+
+var numrep = prompt("Introduce numero de repeticiones");
+
+if (Number(numrep) == numrep) {
+  if (numrep > 0 && numrep <= 50) {
+    var rep, i;
+
+    for (i = 0; i <= numrep; i++) {
+      for (rep = 1; rep <= i; rep++) {
+        document.write(rep);
+      }
+      document.write("<br>");
+    }
+  } else {
+    alert("El número introducido no es válido");
+  }
+}
+
+//PEDIMOS EL NÚMERO POR TECLADO
+//SI LO INTRODUCIDO ES UN NÚMERO
+//SI ES UN NÚMERO ENTRE 1 Y 50
+//BUCLE QUE VA DESDE 0 HASTA numrep
+//BUCLE DESDE 1 HASTA i
