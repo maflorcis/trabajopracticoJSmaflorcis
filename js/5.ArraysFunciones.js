30485c48ac14427b8d0496ebd64da3b91256bd8c
floralonso <floralonso@gmail.com>
2022-07-16 16:46:34 -0300

    ejercicio dos de funciones

--- js/5.ArraysFunciones.js
+++ js/5.ArraysFunciones.js
@@ -4,3 +4,30 @@ le pasa como argumento. A partir de la cadena que se le pasa, la función
 determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o
 por una mezcla de ambas.
 */
+
+function esMayuscula(letra)
+{
+    return letra === letra.toUpperCase();
+}
+
+function esMinuscula(letra)
+{
+    return letra === letra.toLowerCase();
+}
+
+let miPalabra = prompt('ingresá una frase');
+let letraActual
+
+for(let index = 0; index < miPalabra.length; index++)
+{
+    letraActual = miPalabra.charAt(index);
+    if(esMayuscula(letraActual))
+    {
+        document.write("La letra " + letraActual + " es mayúscula" + '<br>');
+    }
+
+    if(esMinuscula(letraActual))
+    {
+        document.write("La letra " + letraActual + " es minúscula" + '<br>');
+    }       
+}
