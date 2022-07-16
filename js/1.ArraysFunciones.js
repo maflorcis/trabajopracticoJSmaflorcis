0a97de9e1b7f0e6c024e0116aafdb695f61ccf62
floralonso <floralonso@gmail.com>
2022-07-16 11:46:40 -0300

    ejercicioArray1

--- js/1.ArraysFunciones.js
+++ js/1.ArraysFunciones.js
@@ -2,5 +2,14 @@
 1- Crear un array llamado meses y que almacene el nombre de los doce meses del
 año. Mostrar por pantalla en forma de lista  los doce nombres del arreglo.
 */
+let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
 
+//mostrar un arreglo de una lista
+
+document.write(`<ul>`);
+
+for(let indice=0; indice < meses.length; indice ++){
+document.write(`<li>${meses[indice]}</li>`);
+}
+document.write(`</ul>` );
 
