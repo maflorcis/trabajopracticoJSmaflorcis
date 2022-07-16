a6a487f2a30d69fd682cd23c15f6bea9a86318a8
floralonso <floralonso@gmail.com>
2022-07-16 17:10:41 -0300

    último ejercicio

--- js/7.ArraysFunciones.js
+++ js/7.ArraysFunciones.js
@@ -3,3 +3,21 @@
 por pantalla, la creación de la tabla debe ser realizada con una función y mostrar
 solo los resultados del 1 al 10 del número elegido por el usuario.
 */
+
+let j = parseInt(prompt('elegí un número del 1 al 10'))
+function escribirTablaMultiplicar(j){
+
+	document.write("<h2>Tabla de multiplicar del "+ j +"</h2>");
+
+	document.write("<ul>");
+	
+	for(i = 1;i<=10;i++){
+		document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+	}
+
+	document.write("</ul>");
+}
+
+escribirTablaMultiplicar(j);
