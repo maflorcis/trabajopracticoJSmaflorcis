7436784f8e334489d18bf8349a9ad43332c2a0d1
floralonso <floralonso@gmail.com>
2022-07-16 16:10:52 -0300

    ejercicio 1 funciones

--- js/4.ArraysFunciones.js
+++ js/4.ArraysFunciones.js
@@ -3,3 +3,22 @@
 entero y devuelve como resultado una cadena de texto que indica si el número es
 par o impar. Mostrar por pantalla el resultado devuelto por la función.
 */
+
+
+let numero = prompt("Introduce un número entero");
+ 
+let resultado = parImpar(numero);
+
+
+function parImpar(numero) {
+    if(numero % 2 == 0) {
+      return "par";
+    }
+    else {
+      return "impar";
+    }
+  }
+  
+
+alert("El número "+numero+" es "+resultado);
+ 
