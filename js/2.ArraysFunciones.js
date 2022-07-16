9b8a7cef01ce6ef36de656197c191f2a574d6719
floralonso <floralonso@gmail.com>
2022-07-16 14:23:06 -0300

    ejercicio 2

--- js/2.ArraysFunciones.js
+++ js/2.ArraysFunciones.js
@@ -10,3 +10,42 @@ debe mostrar el arreglo generado, luego realizar las siguientes acciones:
 ● Sustituye el elemento que ocupa la segunda posición por la ciudad de
 'Barcelona'.
 */
+
+
+let miLista=new Array();
+
+do{
+
+    let ciudad = prompt("Introduce una ciudad");
+    miLista.push('<br>'+ ciudad);
+
+}while((confirm('Desea ingresar una ciudad')))
+
+document.write(miLista)
+
+// Mostrar la longitud del arreglo.
+
+document.write('<br>' + 'La longitud de mi arreglo es: '+ miLista.length + '<br>');
+
+// Mostrar los ítems de las posiciones primera, tercera y última.
+
+
+document.write('<br>' + 'La posición 1  es: '+ miLista[0] + '<br>');
+document.write('<br>' + 'La posición 3 es: '+ miLista[2] + '<br>');
+document.write('<br>' + 'La posición última es: '+ miLista[miLista.length-1] + '<br>');
+
+//Añade en última posición la ciudad de París.
+
+miLista.push('<br>'+'París');
+
+document.write(miLista)
+
+// Escribe por pantalla el elemento que ocupa la segunda posición.
+
+document.write('<br>' + 'La posición 2  es: '+ miLista[1] + '<br>');
+
+// Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'
+
+miLista.splice(1, 0, '<br>'+'Barcelona')
+
+document.write(miLista)
