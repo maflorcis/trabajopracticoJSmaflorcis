f57db825dfa0ec68b6899c69889c70b6bfb9bc19
floralonso <floralonso@gmail.com>
2022-07-16 17:04:28 -0300

    ejercicio de perimetro

--- js/6.ArraysFunciones.js
+++ js/6.ArraysFunciones.js
@@ -3,3 +3,17 @@
 luego crear una función para calcular su perímetro y mostrarlo por pantalla.
 ● La fórmula del perímetro  es p = 2*(a +b)
 */
+
+
+lado1 = parseInt(prompt('ingresá un número que sea la medida de un lado de un rectángulo'))
+lado2 = parseInt(prompt('ingresá un número que sea la medida de otro lado de un rectángulo'))
+
+
+function perimetro(lado1,lado2){
+  
+    let resultado= 2*(lado1 + lado2);
+    alert('El perímetro es: '+ resultado);
+  }
+  
+  perimetro(lado1,lado2);
+
